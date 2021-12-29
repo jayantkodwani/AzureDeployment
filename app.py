3d99eebf07fbd6183095edbba68731794f501f41
Jayant Kodwani <JAYANT.KODWANI@YAHOO.IN>
2021-12-29 08:41:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
+print('Hello Jayant')
 import numpy as np
 from flask import Flask, request, jsonify, render_template, url_for
-import pickle
-
 
 app = Flask(__name__)
 
@@ -10,6 +9,22 @@ def home():
     #return 'Hello World'
     return render_template('home.html')
     #return render_template('index.html')
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    #output = round(prediction[0], 2)
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    #data = request.get_json(force=True)
+    #prediction = model.predict([np.array(list(data.values()))])
+
+    output = 'Hello'
+    return jsonify(output)
 
 if __name__ == '__main__':
     app.run(debug=True)
